45aa7ae5c2296442174659305a82983be997a1eb
mkotra <kotra.michal@gmail.com>
2023-12-22 23:16:17 +0100

    example 02

--- 01_hello/hello.asm
+++ 01_hello/hello.asm
@@ -1,6 +1,7 @@
 ; hello.asm
 section .data
-    msg db      "hello, word",0
+    msg		db      "hello, world",0
+    NL  	db	0xa ; ascii code for new line 
 section .bss
 section .text
     global main
@@ -10,6 +11,11 @@ main:
     mov     rsi, msg        ; string to display in rsi
     mov     rdx, 12         ; length of the string without 0
     syscall                 ; display the string
+    mov     rax, 1          ; 1 = write
+    mov     rdi, 1          ; 1 = stdout
+    mov     rsi, NL         ; display new line
+    mov     rdx, 1          ; length of the string
+    syscall                 ; display the string 
     mov     rax, 60         ; 60 = exit
     mov     rdi, 0          ; 0 = success exit code
     syscall                 ; quit
